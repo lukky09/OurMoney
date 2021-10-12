8407782f675fbf03e71642451168ac2ef33d0645
CoconutWave <rv_nugroho25@yahoo.co.id>
2021-10-12 19:22:12 +0700

    Mbuat Activity pertama Demi UTS Siap Soro

--- app/src/main/java/com/example/ourmoney/MainActivity.java
+++ app/src/main/java/com/example/ourmoney/MainActivity.java
@@ -2,13 +2,118 @@ package com.example.ourmoney;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Trace;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Wallet> daftarwallet;
+    ArrayList<Category> daftarkategorikeluar, daftarkategorimasuk;
+    Button baddtransaction;
+    Spinner spinnertag, spinnerwallet;
+    EditText etduit;
+    TextView tvdata;
+    RadioButton rbkeluar, rbmasuk;
+    ArrayAdapter adaptercategorykeluar,adaptercategorymasuk,adapterwallet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        daftarwallet = new ArrayList<Wallet>();
+        daftarkategorikeluar = new ArrayList<Category>();
+        daftarkategorimasuk = new ArrayList<Category>();
+        etduit = findViewById(R.id.tvtransaksi);
+        baddtransaction = findViewById(R.id.btambahtransaksi);
+        spinnertag = findViewById(R.id.spinnertag);
+        spinnerwallet = findViewById(R.id.spinnerwallet);
+        tvdata = findViewById(R.id.tvdata);
+        rbkeluar = findViewById(R.id.rkeluar);
+        rbmasuk = findViewById(R.id.rdapat);
+
+        spinnerwallet.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                refreshdata();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        daftarwallet.add(new Wallet("Wallet1", 0, new ArrayList<MoneyTransaction>()));
+        daftarwallet.add(new Wallet("Wallet2", 0, new ArrayList<MoneyTransaction>()));
+
+        daftarkategorikeluar.add(new Category("Makanan", "Pengeluaran"));
+        daftarkategorikeluar.add(new Category("Minuman", "Pengeluaran"));
+
+        daftarkategorimasuk.add(new Category("Gaji", "Pemasukan"));
+        daftarkategorimasuk.add(new Category("Bonus", "Pemasukan"));
+
+        adaptercategorykeluar = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, daftarkategorikeluar);
+        adaptercategorykeluar.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        adaptercategorymasuk = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, daftarkategorimasuk);
+        adaptercategorymasuk.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        adapterwallet = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, daftarwallet);
+        adapterwallet.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        spinnerwallet.setAdapter(adapterwallet);
+
+        //Intent i = new Intent(MainActivity.this, );
+
+    }
+
+    //BottomNavigationView.OnNavigationItemSelectedListener(){
+
+    //}
+
+    public void addtransaksi(View view) {
+        MoneyTransaction m;
+        if(rbkeluar.isChecked()){
+            m = new MoneyTransaction(Integer.parseInt(etduit.getText().toString()), rbkeluar.getText().toString(), daftarkategorikeluar.get(spinnertag.getSelectedItemPosition()));
+        }else{
+            m = new MoneyTransaction(Integer.parseInt(etduit.getText().toString()), rbmasuk.getText().toString(), daftarkategorimasuk.get(spinnertag.getSelectedItemPosition()));
+        }
+        daftarwallet.get(spinnerwallet.getSelectedItemPosition()).addTransaction(m);
+        refreshdata();
+    }
+
+    public void refreshdata(){
+        tvdata.setText("");
+        for (int i = 0; i < daftarwallet.get(spinnerwallet.getSelectedItemPosition()).getWalletTransactions().size(); i++) {
+            int tempint = daftarwallet.get(spinnerwallet.getSelectedItemPosition()).getWalletTransactions().get(i).getTransactionAmount();
+            String tempst = daftarwallet.get(spinnerwallet.getSelectedItemPosition()).getWalletTransactions().get(i).getTransactionType();
+            tvdata.setText(tvdata.getText().toString()+""+tempint+"-"+tempst+"\n");
+        }
+
+
+    }
+
+    public void klikradio(View view) {
+        if(view.getId() == R.id.rkeluar){
+            spinnertag.setAdapter(adaptercategorykeluar);
+        }else{
+            spinnertag.setAdapter(adaptercategorymasuk);
+        }
     }
 }
---
+++ app/src/main/java/com/example/ourmoney/Wallet.java
@@ -0,0 +1,52 @@
+package com.example.ourmoney;
+
+import java.util.ArrayList;
+
+public class Wallet {
+    private String walletName;
+    private int walletAmount;
+    private ArrayList<MoneyTransaction> walletTransactions;
+
+    @Override
+    public String toString() {
+        return  walletName;
+    }
+
+    public void addTransaction(MoneyTransaction newtr){
+        walletTransactions.add(newtr);
+        walletAmount = 0;
+        for (int i = 0; i < walletTransactions.size(); i++) {
+            walletAmount += walletTransactions.get(i).getTransactionAmount();
+        }
+    }
+
+    public String getWalletName() {
+        return walletName;
+    }
+
+    public void setWalletName(String walletName) {
+        this.walletName = walletName;
+    }
+
+    public int getWalletAmount() {
+        return walletAmount;
+    }
+
+    public void setWalletAmount(int walletAmount) {
+        this.walletAmount = walletAmount;
+    }
+
+    public ArrayList<MoneyTransaction> getWalletTransactions() {
+        return walletTransactions;
+    }
+
+    public void setWalletTransactions(ArrayList<MoneyTransaction> walletTransactions) {
+        this.walletTransactions = walletTransactions;
+    }
+
+    public Wallet(String walletName, int walletAmount, ArrayList<MoneyTransaction> walletTransactions) {
+        this.walletName = walletName;
+        this.walletAmount = walletAmount;
+        this.walletTransactions = walletTransactions;
+    }
+}
